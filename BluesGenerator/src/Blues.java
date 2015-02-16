d56e2ed7a1465a4caa15372ac1055f6f589ae982
Alistair Madden <phantommelon@gmail.com>
2015-02-16 22:05:16 +0000

    Add Blues class
    The Blues class generates a blues song with predefined nouns,
    adjectives and such.
    
    Implement RandomAuthor method to generate an author when none is given
    as arguments on the command line.
    
    Add extra constructor for implementing an author. Todo - add code to
    make this work in the main method.

---
+++ BluesGenerator/src/Blues.java
@@ -0,0 +1,276 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * A class to sing the blues (not that anyone singing the blues would have
+ * access to this program).
+ * 
+ * @author Alistair Madden <phantommelon@gmail.com> 
+ * @version 1.0
+ */
+
+public class Blues {
+    
+    private ArrayList<String> nouns, verbs, adjectives;
+    private String author;
+    private ArrayList<String> first;
+    private ArrayList<String> middle;
+    private ArrayList<String> last;
+    private Random rand = new Random();
+    
+    public Blues() {
+        nouns = new ArrayList(30);
+        nouns.add("sun");
+        nouns.add("morning");
+        nouns.add("baby");
+        nouns.add("woman");
+        nouns.add("guy");
+        nouns.add("down");
+        nouns.add("highway");
+        nouns.add("behind");
+        nouns.add("trees");
+        nouns.add("heart");
+        nouns.add("someone");
+        nouns.add("wind");
+        nouns.add("pine");
+        nouns.add("top");
+        nouns.add("love");
+        nouns.add("rooster");
+        nouns.add("day");
+        nouns.add("farm");
+        nouns.add("yard");
+        nouns.add("dog");
+        nouns.add("hound");
+        nouns.add("hobo");
+        nouns.add("home");
+        nouns.add("peace");
+        nouns.add("crossroads");
+        nouns.add("knees");
+        nouns.add("ride");
+        nouns.add("side");
+        
+        verbs = new ArrayList(20);
+        verbs.add("went");
+        verbs.add("fell");
+        verbs.add("asked");
+        verbs.add("save");
+        verbs.add("saved");
+        verbs.add("flag");
+        verbs.add("flagged");
+        verbs.add("passed");
+        verbs.add("going");
+        verbs.add("gonna");
+        verbs.add("rollin");
+        verbs.add("leavin");
+        verbs.add("headin");
+        verbs.add("go");
+        verbs.add("crow");
+        verbs.add("howl");
+        verbs.add("settin");
+        verbs.add("rose");
+        
+        adjectives = new ArrayList(20);
+        adjectives.add("big");
+        adjectives.add("lonesome");
+        adjectives.add("miserable");
+        adjectives.add("lazy");
+        adjectives.add("strange");
+        adjectives.add("red");
+        adjectives.add("little");
+        adjectives.add("dark");
+        adjectives.add("small");
+        adjectives.add("wet");
+        adjectives.add("cold");
+        adjectives.add("lonely");
+        adjectives.add("crazy");
+        adjectives.add("poorly");
+        adjectives.add("cheap");
+        adjectives.add("wasted");
+        
+        first = new ArrayList(10);
+        first.add("Blind");
+        first.add("Cripple");
+        first.add("Lame");
+        first.add("Maimed");
+        first.add("Poor");
+        first.add("Infirmed");
+        first.add("Deaf");
+        first.add("Dead");
+        first.add("Deceased");
+        
+        
+        middle = new ArrayList(10);
+        middle.add("Lemon");
+        middle.add("Lime");
+        middle.add("Pear");
+        middle.add("Joe");
+        middle.add("Willie");
+        middle.add("Sadie");
+        middle.add("Big Mama");
+        middle.add("Bessie");
+        middle.add("Fat River Dumpling");
+        middle.add("Orange");
+        middle.add("Pineapple");
+        
+        
+        last = new ArrayList(10);
+        last.add("Obama");
+        last.add("Clinton");
+        last.add("Bush");
+        last.add("Reagan");
+        last.add("Carter");
+        last.add("Truman");
+        last.add("Kennedy");
+        last.add("Roosevelt");
+        last.add("Hoover");
+        last.add("McKinley");
+        last.add("Johnson");
+        
+        author = RandomAuthor();
+    }
+    
+    public Blues(String author) {
+        nouns = new ArrayList(30);
+        nouns.add("sun");
+        nouns.add("morning");
+        nouns.add("baby");
+        nouns.add("woman");
+        nouns.add("guy");
+        nouns.add("down");
+        nouns.add("highway");
+        nouns.add("behind");
+        nouns.add("trees");
+        nouns.add("heart");
+        nouns.add("someone");
+        nouns.add("wind");
+        nouns.add("pine");
+        nouns.add("top");
+        nouns.add("love");
+        nouns.add("rooster");
+        nouns.add("day");
+        nouns.add("farm");
+        nouns.add("yard");
+        nouns.add("dog");
+        nouns.add("hound");
+        nouns.add("hobo");
+        nouns.add("home");
+        nouns.add("peace");
+        nouns.add("crossroads");
+        nouns.add("knees");
+        nouns.add("ride");
+        nouns.add("side");
+        
+        verbs = new ArrayList(20);
+        verbs.add("went");
+        verbs.add("fell");
+        verbs.add("asked");
+        verbs.add("save");
+        verbs.add("saved");
+        verbs.add("flag");
+        verbs.add("flagged");
+        verbs.add("passed");
+        verbs.add("going");
+        verbs.add("gonna");
+        verbs.add("rollin");
+        verbs.add("leavin");
+        verbs.add("headin");
+        verbs.add("go");
+        verbs.add("crow");
+        verbs.add("howl");
+        verbs.add("settin");
+        verbs.add("rose");
+        
+        adjectives = new ArrayList(20);
+        adjectives.add("big");
+        adjectives.add("lonesome");
+        adjectives.add("miserable");
+        adjectives.add("lazy");
+        adjectives.add("strange");
+        adjectives.add("red");
+        adjectives.add("little");
+        adjectives.add("dark");
+        adjectives.add("small");
+        adjectives.add("wet");
+        adjectives.add("cold");
+        adjectives.add("lonely");
+        adjectives.add("crazy");
+        adjectives.add("poorly");
+        adjectives.add("cheap");
+        adjectives.add("wasted");
+        
+        first = new ArrayList(10);
+        first.add("Blind");
+        first.add("Cripple");
+        first.add("Lame");
+        first.add("Maimed");
+        first.add("Poor");
+        first.add("Infirmed");
+        first.add("Deaf");
+        first.add("Dead");
+        first.add("Deceased");
+        
+        
+        middle = new ArrayList(10);
+        middle.add("Lemon");
+        middle.add("Lime");
+        middle.add("Pear");
+        middle.add("Joe");
+        middle.add("Willie");
+        middle.add("Sadie");
+        middle.add("Big Mama");
+        middle.add("Bessie");
+        middle.add("Fat River Dumpling");
+        middle.add("Orange");
+        middle.add("Pineapple");
+        
+        
+        last = new ArrayList(10);
+        last.add("Obama");
+        last.add("Clinton");
+        last.add("Bush");
+        last.add("Reagan");
+        last.add("Carter");
+        last.add("Truman");
+        last.add("Kennedy");
+        last.add("Roosevelt");
+        last.add("Hoover");
+        last.add("McKinley");
+        last.add("Johnson");
+        
+        this.author = author;
+    }
+
+    private String RandomAuthor() {
+        
+        String name = new String();
+        
+        name += first.get((int) (rand.nextFloat()*first.size()));
+        name += " ";
+        name += middle.get((int) (rand.nextFloat()*middle.size()));
+        name += " ";
+        name += last.get((int) (rand.nextFloat()*last.size()));
+        
+        return name;
+        
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public static void main(String[] args) {
+        
+        if(args.length == 0) {
+            Blues song = new Blues();
+            System.out.println(song.getAuthor());
+        }
+        
+    }
+}
